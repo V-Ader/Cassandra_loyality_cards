a2685ecc44ec615ebf721ffc7e0e168832233466
Beata Wiśniewska <beatawis20@gmail.com>
2025-01-16 21:09:54 +0100

    Merge changes

--- src/main/java/loyalty/Main.java
+++ src/main/java/loyalty/Main.java
@@ -6,22 +6,24 @@ import loyalty.database.ConnectionException;
 
 public class Main {
     public static void main(String[] args) {
-        ClientSession session;
+        ClientSession clientSession;
         String clientEmail = "owner1@example.com";
         String issuerEmail = "issuer1@example.com";
-        IssuerSession session;
+        IssuerSession issuerSession;
         try {
-            session = new ClientSession(clientEmail, issuerEmail);
-            session = new IssuerSession("karol@zawislak.pl");
+            clientSession = new ClientSession(clientEmail, issuerEmail);
+            issuerSession = new IssuerSession("karol@zawislak.pl");
         } catch (ConnectionException e) {
             throw new RuntimeException(e);
         }
         System.out.println("Client connected!");
 
-        session.createCard("test2@gmail.com", 20);
-        session.getAllCards();
+        issuerSession.createCard("test2@gmail.com", 20);
+        issuerSession.getAllCards();
 
-        session.closeConnection();
+        clientSession.closeConnection();
+        issuerSession.closeConnection();
+
         System.out.println("Client disconnected!");
 
     }
