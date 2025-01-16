10585123a04d9df867832cc8900576aec21575df
Karol Zawiślak <56037641+V-Ader@users.noreply.github.com>
2025-01-16 15:53:26 +0100

    feat(issuer): add basic issuer service

--- src/main/java/loyalty/Main.java
+++ src/main/java/loyalty/Main.java
@@ -1,18 +1,20 @@
 package loyalty;
 
-import loyalty.actors.client.ClientSession;
+import loyalty.actors.issuer.IssuerSession;
 import loyalty.database.ConnectionException;
 
 public class Main {
     public static void main(String[] args) {
-        ClientSession session;
+        IssuerSession session;
         try {
-            session = new ClientSession();
+            session = new IssuerSession("karol@zawislak.pl");
         } catch (ConnectionException e) {
             throw new RuntimeException(e);
         }
         System.out.println("Client connected!");
 
+        session.createCard("test2@gmail.com", 20);
+        session.getAllCards();
 
         session.closeConnection();
         System.out.println("Client disconnected!");
